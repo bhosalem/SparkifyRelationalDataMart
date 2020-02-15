b19ac9b67df257976b36a4cc10206ba1c5722dc3
Machchindranath Bhosale <38831000+bhosalem@users.noreply.github.com>
2020-02-15 17:12:15 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -83,7 +83,8 @@ def process_log_file(cur, filepath):
             songid, artistid = results
         else:
             songid, artistid = None, None
-
+        
+            
         # insert songplay record
         songplay_data = (pd.to_datetime(row.ts,unit ='ms'),row.userId,row.level,songid,artistid,row.sessionId,row.location,row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
