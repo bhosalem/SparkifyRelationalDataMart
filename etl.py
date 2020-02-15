3aec3442e444d9e9cdaf0c7bfb9469f4a03b2259
bhosalem <38831000+bhosalem@users.noreply.github.com>
2020-02-15 15:02:26 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -6,38 +6,67 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """Load Data into the song and artist table
+       For each of the file identifed in the process data table,reads 
+       the json file into pandas dataframe extract the required data 
+       columns from the file and insert into the song and artist tables.
+       
+       Parameters:
+       cur object : cursor object defined for sparkify connection in main()
+       filepath (string): Path to the song/log data files
+       
+       Returns : none
+    """
     # open song file
-    df = 
+    df = pd.read_json(filepath,lines=True)
 
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    song_data = df.loc[:,['song_id','title','artist_id','year','duration']].values.tolist()
+    for row in song_data:
+        cur.execute(song_table_insert, row)
     
     # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    artist_data =df.loc[:,['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values.tolist()
+    for row in artist_data:
+        cur.execute(artist_table_insert, row)
 
 
 def process_log_file(cur, filepath):
+    """Load data into the users and time and sonplay fact table
+    
+       For each of the file identifed in the process data table,
+       reads thee json file into pandas dataframe,extract the 
+       required data columns from the file and insert into the 
+       users and time tables.
+       
+       Function also derives the artist_id and song_id keys for 
+       inserts into fact table songplays
+       
+       Parameters:
+       cur object : cursor object defined for sparkify connection in main()
+       filepath (string): Path to the song/log data files
+       
+       Returns : none
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath,lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] =='NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'],unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (t.dt.time,t.dt.hour,t.dt.day,t.dt.week,t.dt.month,t.dt.year,t.dt.weekday)
+    column_labels = ('start_time','hour','day','week','month','year','weekday')
+    time_df = pd.DataFrame.from_dict(dict(zip(column_labels,time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df.loc[:,['userId','firstName','lastName','gender','level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -47,15 +76,32 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (pd.to_datetime(row.ts,unit ='ms'),row.userId,row.level,songid,artistid,row.sessionId,row.location,row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """Derives files present in the filepath, displays the total
+       number of files fount in the supplied filepath, invokes
+       function defined by func parameter
+       
+       Parameters:
+       cur object : cursor object defined for sparkify connection in main()
+       conn object : connection object defined to connect to sparkify DB in main()
+       filepath (string): Path to the song/log data files
+       func (string): Name of the function to call based on data file
+       
+       Returns : none
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -75,12 +121,17 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """This is main function which connects to sparkify database and 
+       then calls process data for song data and log data separately
+       to load all tables and at the end close the connection.
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
-
+    cur.execute(create_songplay_seq)
+    cur.execute(create_time_seq)
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
+    conn.commit()
     conn.close()
 
 
